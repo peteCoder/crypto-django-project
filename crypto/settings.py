dcc8e742dc0f781554306c55f6aaf8d14f5df883
tslnigeriatv <57802488+tslnigeriatv@users.noreply.github.com>
2023-05-29 15:20:57 +0100

    changed the runtime to python3.9

--- crypto/settings.py
+++ crypto/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '.vercel.app', 
+    'localhost',
 ]
 
 
--- crypto/urls.py
+++ crypto/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,3 +28,7 @@ urlpatterns = [
     # Dashboard
     path('dashboard/', include("dashboard.urls")),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
